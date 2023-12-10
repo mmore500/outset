0102b79e68aa73694c20501758f98cb3e9bc8a73
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-12-10 15:30:49 -0500

    Impl, test mark_magnifying_glass

--- src/outset/__init__.py
+++ src/outset/__init__.py
@@ -4,7 +4,9 @@ from __future__ import annotations
 __version__ = "0.0.0"
 
 from outset.draw_outset_ import draw_outset
+from outset.mark_magnifying_glass_ import mark_magnifying_glass
 
 __all__ = [
     "draw_outset",
+    "mark_magnifying_glass",
 ]
---
+++ tests/test_mark_magnifying_glass.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+
+from outset import mark_magnifying_glass
+
+
+def test_mark_magnifying_glass_single():
+    plt.clf()  # Clear the current figure
+    ax = plt.gca()  # Get the current axes
+
+    # Call the function with test values
+    mark_magnifying_glass(x=0.5, y=0.5, ax=ax, color="red")
+
+    # Save and print the path of the output image
+    outpath = "/tmp/test_mark_magnifying_glass_single.png"
+    plt.savefig(outpath)
+    print(f"saved graphic to {outpath}")
+    plt.close()  # Close the plot after saving
+
+
+def test_mark_magnifying_glass_multiple():
+    plt.clf()
+    # Multiple calls to the function with different parameters
+    mark_magnifying_glass(x=0.3, y=0.3)
+    mark_magnifying_glass(x=0.7, y=0.7, color="blue", accent_color="orange")
+
+    # Save and print the path of the output image
+    outpath = "/tmp/test_mark_magnifying_glass_multiple.png"
+    plt.savefig(outpath)
+    print(f"saved graphic to {outpath}")
+    plt.close()
