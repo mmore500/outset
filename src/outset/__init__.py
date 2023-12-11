c5fde8f10849e5611b1bbc4a1c8f896d087f2e7d
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-12-10 21:22:16 -0500

    Impl, test draw_arrow helper

--- src/outset/__init__.py
+++ src/outset/__init__.py
@@ -4,11 +4,13 @@ from __future__ import annotations
 __version__ = "0.0.0"
 
 from outset.draw_outset_ import draw_outset
+from outset.mark_arrow_ import mark_arrow
 from outset.mark_inlaid_asterisk_ import mark_inlaid_asterisk
 from outset.mark_magnifying_glass_ import mark_magnifying_glass
 
 __all__ = [
     "draw_outset",
+    "mark_arrow",
     "mark_inlaid_asterisk",
     "mark_magnifying_glass",
 ]
---
+++ tests/test_mark_arrow.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+
+from outset import mark_arrow
+
+
+def test_mark_arrow_single():
+    plt.clf()  # Clear the current figure
+    ax = plt.gca()  # Get the current axes
+
+    # Call the function with test values
+    mark_arrow(x=0.5, y=0.5, ax=ax, color="red")
+
+    # Save and print the path of the output image
+    outpath = "/tmp/test_mark_arrow_single.png"
+    plt.savefig(outpath)
+    print(f"saved graphic to {outpath}")
+    plt.close()  # Close the plot after saving
+
+
+def test_mark_arrow_multiple():
+    plt.clf()
+    # Multiple calls to the function with different parameters
+    mark_arrow(x=0.3, y=0.3, color="lavender")
+    mark_arrow(x=0.7, y=0.7, color="blue", accent_color="orange")
+
+    # Save and print the path of the output image
+    outpath = "/tmp/test_mark_arrow_multiple.png"
+    plt.savefig(outpath)
+    print(f"saved graphic to {outpath}")
+    plt.close()
