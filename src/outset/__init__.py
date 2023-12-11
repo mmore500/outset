788a303c631498c233cffd9c5079a78cd2b8e8e5
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-12-11 00:12:54 -0500

    Make imports relative instead of absolute

--- src/outset/__init__.py
+++ src/outset/__init__.py
@@ -3,10 +3,10 @@ from __future__ import annotations
 
 __version__ = "0.0.0"
 
-from outset.draw_outset_ import draw_outset
-from outset.mark_arrow_ import mark_arrow
-from outset.mark_inlaid_asterisk_ import mark_inlaid_asterisk
-from outset.mark_magnifying_glass_ import mark_magnifying_glass
+from .draw_outset_ import draw_outset
+from .mark_arrow_ import mark_arrow
+from .mark_inlaid_asterisk_ import mark_inlaid_asterisk
+from .mark_magnifying_glass_ import mark_magnifying_glass
 
 __all__ = [
     "draw_outset",
