4078f2eee08e0d37b4ed4c97fcc8a5a2e3e2078d
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-12-12 14:54:04 -0500

    Test, fixup is_axes_unset

--- src/outset/_auxlib/is_axes_unset_.py
+++ src/outset/_auxlib/is_axes_unset_.py
@@ -5,7 +5,7 @@ def is_axes_unset(ax: mpl_Axes) -> bool:
     """Test if axes limits have been set."""
     # see https://matplotlib.org/stable/users/faq.html#check-whether-a-figure-is-empty
     return not (
-        len(ax.get_children()) > 11  # 11 objs in empty ax
+        len(ax.get_children()) > 10  # 10 objs in empty ax
         or ax.get_xlim() != (0.0, 1.0)  # in case axlim set manually...
         or ax.get_ylim() != (0.0, 1.0)
     )
---
+++ tests/test_auxlib/test_is_axes_unset.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+
+from outset._auxlib.is_axes_unset_ import is_axes_unset
+
+
+def test_empty_axis():
+    fig, ax = plt.subplots()
+    assert is_axes_unset(ax) is True
+    plt.close(fig)
+
+
+def test_empty_axis_gca():
+    plt.clf()
+    assert is_axes_unset(plt.gca()) is True
+
+
+def test_axis_with_children():
+    fig, ax = plt.subplots()
+    ax.plot([0, 1], [0, 1])  # Add a line plot to the axis
+    ax.set_xlim(0, 1)
+    ax.set_ylim(0, 1)
+    assert is_axes_unset(ax) is False
+    plt.close(fig)
+
+
+def test_manually_set_limits():
+    fig, ax = plt.subplots()
+    ax.set_xlim(0.0, 2.0)
+    assert is_axes_unset(ax) is False
+    plt.close(fig)
+
+
+def test_auto_set_limits():
+    fig, ax = plt.subplots()
+    ax.plot([0, 1], [0, 2])  # Add a line plot to the axis
+    assert is_axes_unset(ax) is False
+    plt.close(fig)
