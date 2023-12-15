f41459f91f66957d05f374be99011fe93331858c
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-12-14 19:24:26 -0500

    Add physical aspect calc mode

--- src/outset/_auxlib/calc_aspect_.py
+++ src/outset/_auxlib/calc_aspect_.py
@@ -2,6 +2,33 @@ from matplotlib import axes as mpl_axes
 import numpy as np
 
 
-def calc_aspect(ax: mpl_axes.Axes) -> float:
-    """Calculate the ratio between ylim span length and xlim span length."""
-    return np.ptp(ax.get_ylim()) / np.ptp(ax.get_xlim())
+def calc_aspect(ax: mpl_axes.Axes, physical: bool = False) -> float:
+    """Calculate the aspect ratio of the axes.
+
+    If physical is True, the aspect ratio is calculated based on the size ratio
+    between unit values unit on each axis after rendering transforms. If
+    physical is False, the aspect ratio is based on the axis viewport limits.
+    """
+    if physical:
+        # Transform a point one unit right and one unit up from the origin
+        origin = ax.transData.transform((0, 0))
+        one_unit_right = ax.transData.transform((1, 0))
+        one_unit_up = ax.transData.transform((0, 1))
+
+        # Calculate the distance in pixels for one unit on each axis
+        dx = np.sqrt(
+            (one_unit_right[0] - origin[0]) ** 2
+            + (one_unit_right[1] - origin[1]) ** 2
+        )
+        dy = np.sqrt(
+            (one_unit_up[0] - origin[0]) ** 2
+            + (one_unit_up[1] - origin[1]) ** 2
+        )
+
+        # Calculate the physical aspect ratio
+        aspect_ratio = dx / dy
+    else:
+        # Calculate the aspect ratio based on data limits.
+        aspect_ratio = np.ptp(ax.get_ylim()) / np.ptp(ax.get_xlim())
+
+    return aspect_ratio
--- tests/test_auxlib/test_calc_aspect.py
+++ tests/test_auxlib/test_calc_aspect.py
@@ -1,21 +1,55 @@
 from matplotlib import pyplot as plt
+import numpy as np
 
 from outset._auxlib.calc_aspect_ import calc_aspect
 
 
+def test_calc_aspect_square_physical():
+    fig, ax = plt.subplots(figsize=(6, 6))  # Create a 6x6 inch figure
+    ax.set_xlim(0, 1)
+    ax.set_ylim(0, 1)
+    plt.draw()  # Important to update the figure layout
+    assert 0.95 < calc_aspect(ax, physical=True) < 1.05
+    assert calc_aspect(ax) == 1
+
+
+def test_calc_aspect_tall_physical():
+    fig, ax = plt.subplots(figsize=(6, 12))  # Create a 6x12 inch figure
+    ax.set_xlim(0, 1)
+    ax.set_ylim(0, 1)
+    plt.draw()  # Important to update the figure layout
+    assert abs(calc_aspect(ax, physical=True) - 0.5) < 0.1
+    assert calc_aspect(ax) == 1
+
+
+def test_calc_aspect_wide_physical():
+    fig, ax = plt.subplots(figsize=(12, 6))  # Create a 12x6 inch figure
+    ax.set_xlim(0, 1)
+    ax.set_ylim(0, 1)
+    plt.draw()  # Important to update the figure layout
+    assert abs(calc_aspect(ax, physical=True) - 2) < 0.1
+    assert calc_aspect(ax) == 1
+
+
 def test_calc_aspect_square():
-    plt.gca().set_xlim(0, 1)
-    plt.gca().set_ylim(0, 1)
-    assert calc_aspect(plt.gca()) == 1
+    fig, ax = plt.subplots(figsize=(6, 6))  # Create a 6x6 inch figure
+    ax.set_xlim(0, 1)
+    ax.set_ylim(0, 1)
+    assert calc_aspect(ax) == 1
+    assert abs(calc_aspect(ax, physical=True) - 1) < 0.1
 
 
 def test_calc_aspect_tall():
-    plt.gca().set_xlim(0, 1)
-    plt.gca().set_ylim(0, 2)
-    assert calc_aspect(plt.gca()) == 2
+    fig, ax = plt.subplots(figsize=(6, 6))  # Create a 6x6 inch figure
+    ax.set_xlim(0, 1)
+    ax.set_ylim(0, 2)
+    assert calc_aspect(ax) == 2
+    assert abs(calc_aspect(ax, physical=True) - 2) < 0.1
 
 
 def test_calc_aspect_wide():
-    plt.gca().set_xlim(-1, 3)
-    plt.gca().set_ylim(0, 2)
-    assert calc_aspect(plt.gca()) == 0.5
+    fig, ax = plt.subplots(figsize=(6, 6))  # Create a 6x6 inch figure
+    ax.set_xlim(-1, 3)
+    ax.set_ylim(0, 2)
+    assert calc_aspect(ax) == 0.5
+    assert abs(calc_aspect(ax, physical=True) - 0.5) < 0.1
