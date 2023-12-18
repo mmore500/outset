caa2e6143ef0f59af7af29e8ef5958b212e68a33
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-12-18 15:49:49 -0500

    Add hueless example

--- tests/test_make_examples.py
+++ tests/test_make_examples.py
@@ -6,7 +6,37 @@ import outset
 
 
 @pytest.mark.integration
-def test_make_example_hue_col_outset():
+def test_make_example_hueless():
+    og = outset.OutsetGrid(
+        data=sns.load_dataset("iris").dropna(),
+        x="petal_width",
+        y="petal_length",
+        col="species",
+        col_wrap=2,
+        color=sns.color_palette()[1],
+        marqueeplot_kwargs={
+            "mark_glyph": outset.MarkAlphabeticalBadges,
+        },
+    )
+    og.map_dataframe(
+        sns.kdeplot, x="petal_width", y="petal_length", legend=False, zorder=0
+    )
+    og.map_dataframe_outset(
+        sns.scatterplot,
+        x="petal_width",
+        y="petal_length",
+        legend=False,
+        zorder=0,
+    )
+    og.marqueeplot()
+
+    outpath = "/tmp/test_make_example_hueless.png"
+    plt.savefig(outpath)
+    print(f"saved graphic to {outpath}")
+
+
+@pytest.mark.integration
+def test_make_example_huefull():
     og = outset.OutsetGrid(
         data=sns.load_dataset("penguins").dropna(),
         x="bill_length_mm",
@@ -20,6 +50,6 @@ def test_make_example_hue_col_outset():
     og.marqueeplot()
     og.add_legend()
 
-    outpath = "/tmp/test_make_example_hue_col_outset.png"
+    outpath = "/tmp/test_make_example_huefull.png"
     plt.savefig(outpath)
     print(f"saved graphic to {outpath}")
