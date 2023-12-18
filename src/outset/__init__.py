47361136a1178cd6d2bda5cf706134927b06fdd6
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-12-17 19:59:29 -0500

    Impleent, test inset_outsets

--- src/outset/__init__.py
+++ src/outset/__init__.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 __version__ = "0.0.0"
 
 from .draw_marquee_ import draw_marquee
+from .inset_outsets_ import inset_outsets
 from .mark_arrow_ import mark_arrow
 from .mark_inlaid_asterisk_ import mark_inlaid_asterisk
 from .mark_magnifying_glass_ import mark_magnifying_glass
@@ -18,6 +19,7 @@ from .stub_edge_mark_ import stub_edge_mark
 
 __all__ = [
     "draw_marquee",
+    "inset_outsets",
     "mark_arrow",
     "mark_inlaid_asterisk",
     "mark_magnifying_glass",
