150b44de1fd752a0f19c0dba5be4d095ca6f065b
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-12-30 16:34:47 -0500

    Expose calc/set_aspect in util module

--- src/outset/util/__init__.py
+++ src/outset/util/__init__.py
@@ -1,11 +1,15 @@
 """Utility functions."""
 
+from .._auxlib.calc_aspect_ import calc_aspect
+from .._auxlib.set_aspect_ import set_aspect
 from ._layout_corner_insets import layout_corner_insets
 from ._NamedFrames import NamedFrames
 from ._SplitKwarg import SplitKwarg
 
 __all__ = [
+    "calc_aspect",
     "layout_corner_insets",
     "NamedFrames",
+    "set_aspect",
     "SplitKwarg",
 ]
